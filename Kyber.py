1b30455db8c6b5889dd77c407eebba6e0b16dfb6
SamLorenzoSanc <68753976+SamLorenzoSanc@users.noreply.github.com>
2024-12-08 20:53:44 +0000

    [0.0.0.0]

--- Kyber.py
+++ Kyber.py
@@ -979,7 +979,7 @@ def generate_keys():
     })
 
 if __name__ == '__main__':
-    app.run(port=5001)  # Ejecutar el servidor en el puerto 5001
+    app.run(host='0.0.0.0', port=5001)  # Ejecutar el servidor en el puerto 5001
 
 
 # message = b'hola'
